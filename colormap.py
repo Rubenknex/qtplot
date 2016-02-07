af8a60c919b8faa660637ee93476866b3d18ced2
Ruben van Gulik <rubenvangulik@gmail.com>
2016-02-07 18:41:32 +0100

    Converted all colormaps to txt format so they all work

--- colormap.py
+++ colormap.py
@@ -15,26 +15,26 @@ def read_ppm_colormap(filename):
 
 
 class Colormap:
-	def __init__(self, filename):
-		self.colors = read_ppm_colormap(filename)
-		self.gamma = 1
-		self.min, self.max = 0, 1
+    def __init__(self, filename):
+        self.colors = np.loadtxt(filename)
+        self.gamma = 1
+        self.min, self.max = 0, 1
 
-		self.length = 2048
+        self.length = self.colors.shape[0]
 
-	def get_limits(self):
-		return self.min, self.max
+    def get_limits(self):
+        return self.min, self.max
 
-	def get_colors(self):
-		x = np.linspace(0, 1, self.length)
-		y = x**self.gamma
+    def get_colors(self):
+        x = np.linspace(0, 1, self.length)
+        y = x**self.gamma
 
-		colors_x = np.linspace(0, 1, len(self.colors))
-		r = np.interp(y, colors_x, self.colors[:,0])
-		g = np.interp(y, colors_x, self.colors[:,1])
-		b = np.interp(y, colors_x, self.colors[:,2])
-		
-		return np.dstack((r, g, b)).reshape(len(r), 3).astype(np.uint8)
+        colors_x = np.linspace(0, 1, len(self.colors))
+        r = np.interp(y, colors_x, self.colors[:,0])
+        g = np.interp(y, colors_x, self.colors[:,1])
+        b = np.interp(y, colors_x, self.colors[:,2])
+        
+        return np.dstack((r, g, b)).reshape(len(r), 3).astype(np.uint8)
 
-	def get_mpl_colormap(self):
-		return mpl.colors.ListedColormap(self.get_colors().astype(float) / 255.0)
+    def get_mpl_colormap(self):
+        return mpl.colors.ListedColormap(self.get_colors().astype(float) / 255.0)
